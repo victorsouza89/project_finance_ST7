5098765a8eb51ee64ab3d0251d248358f3b1d57c
yanisadel <yanis_adel@hotmail.com>
2022-02-16 15:02:01 +0100

    m

--- main.py
+++ main.py
@@ -8,7 +8,13 @@ path = "DataProjets.xlsx"
 
 """a)"""
 df = pd.read_excel(path, sheet_name="Mapping")
-msft = yf.Ticker(df["Tickers"][0])  # I don't know what to do with this
+
+
+def get_msft(df, sedol):
+    n = len(df)
+    for i in range(n):
+        if str(df["Sedol"][i]) == str(sedol):
+            return yf.Ticker(df["Tickers"][i])
 
 
 """b)"""
@@ -36,18 +42,50 @@ df2 = nettoyage(df2)
 """d)"""
 
 
+def convert_date(date):
+    return str(date.year) + "-" + str(date.month) + "-" + str(date.day)
+
+
+def get_price(msft, date):
+    todays_data = msft.history(period="20y")["Close"]
+
+    return todays_data["Close"][date]
+
+
+def get_rit(df, date, sebol):
+    msft = get_msft(df, sebol)
+    P_i_t = get_price(msft, convert_date(date))
+    P_i_tbefore = get_price(msft, convert_date(date))
+    if (P_i_tbefore) == 0:
+        return -1
+    else:
+        return P_i_t
+
+
+date = df2["date"][226]
+date_convert = convert_date(date)
+
+sebol = df2.columns[1]
+msft = get_msft(df, sebol)
+
+todays_data = msft.history(period="20y")
+close = todays_data["Close"]
+
+get_rit(df, date=date, sebol=sebol)
+
+
 def get_indice(df2, t=100, N=300):
     rt = 0
     columns = df2.columns[1:]
     for i in range(N):
-        # Calcul de r_t^i
         company = df2[columns[i]]
-        r_i_t = 0
-        if company[t - 1] == 0:
-            r_i_t = 0
-        else:
-            r_i_t = company[t] / company[t - 1] - 1
-        rt += r_i_t
+
+        w_t_i = df2[i]
+
+        r_t_i = 0
+        r_t_i = get_rit(df, df2["date"][t])
+
+        rt += w_t_i * r_t_i
 
     return rt
 
