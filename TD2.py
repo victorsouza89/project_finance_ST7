9ddab8768e82b50dcfb59f81f3431cb6aa7dc928
Siméon Laflèchelle <simeon.laflechelle@student-cs.fr>
2022-02-23 15:02:07 +0100

    td2

--- TD2.py
+++ TD2.py
@@ -1,29 +1,50 @@
 import numpy as np
 import matplotlib.pyplot as plt
-delta= np.array([[0.04,0.0192,-0.0024],[0.0192,0.0256,-0.00096],[-0.0024,-0.00096,0.0036]])
-delta1=np.linalg.inv(delta)
-un=np.array([[1],[1],[1]])
-unt=np.transpose(un)
-R=np.array([[0.12],[0.1],[0.06]])
-Rt=np.transpose(R)
-r_list=np.linspace(0,1,100)
+from numpy.linalg import inv
+import cvxpy as cp
 
-A=np.dot(np.dot(unt,delta1),R)
-B=np.dot(np.dot(unt,delta1),un)
-C=np.dot(np.dot(Rt,delta1),R)
-risque=[]
-x1,x2,x3=[],[],[]
-for r in r_list:
-  lamb=(C-A*r)/(A**2-B*C)
-  mu=(A-r*B)/(A**2-B*C)
-  x=np.dot(delta1,(mu*R-lamb*un))
-  x1.append(x[0])
-  x2.append(x[1])
-  x3.append(x[2])
-  risque.append(np.dot(np.dot(np.transpose(x),delta),x)[0][0])
+"""ex1"""
+'a'
 
-plt.plot(risque,r_list)
-plt.show()
+volatility = np.array([[14.3,17.4,21.2,4.3,4,8.4,0.5]])
+mu = np.array([[6,7,9.5,1.5,1.3,3.2,0]])
+rho =  np.array([[1,0.82,0.78,0.1,0,0.5,0],[0.82,1,0.85,0.12,0.08,0.63,0],[0.78,0.85,1,0.05,0.03,0.71,0],[0.1,0.12,0.05,1,0.65,0.2,0],[0,0.08,0.03,0.65,1,0.23,0],[0.5,0.63,0.71,0.2,0.23,1,0],[0,0,0,0,0,0,1]])
 
-plt.plot(r_list,x3)
-plt.show()
+
+def get_delta(rho,volatility):
+    n=len(volatility[0])
+    delta=np.zeros((n,n))
+    for i in range(n):
+        for j in range(n):
+
+            delta[i,j]=rho[i,j]*volatility[0,i]*volatility[0,j]
+    return delta
+
+def weight(volatily=volatility,mu=mu,rho=rho,lambd=0.5):
+    delta=get_delta(rho,volatility)
+    return 1/(2*lambd)*np.dot(mu,inv(delta))
+
+def get_volatility(weight,rho):
+    v=0
+    for i in range(len(weight)):
+        for j in range(len(weight)):
+            v+=weight[i]*weight[j]*rho[i,j]
+    return v
+
+w=weight()
+print("weight")
+print(w)  
+print("volatility")
+print(get_volatility(w,rho))
+
+'ex3'
+n=len(volatility[0])
+delta=get_delta(rho,volatility)
+w = cp.Variable(n)
+gamma = 0.5
+ret = mu@w 
+risk = cp.quad_form(w, delta)
+prob = cp.Problem(cp.Maximize(ret - gamma*risk), 
+               [cp.sum(w) == 1, 
+                w >= 0])
+
