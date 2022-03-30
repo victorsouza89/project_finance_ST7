d2a990c984366c51626aaba8856b5d481f7d50ca
Hugo <hugo.wolff@student-cs.fr>
2022-03-30 11:21:13 +0200

    TD4 suite

--- TD4.py
+++ TD4.py
@@ -11,7 +11,7 @@ import pickle
 all_dates=pickle.load( open( "all_dates.p", "rb" ) )
 perf=pd.read_csv('performance.csv',sep=';')
 volatility = np.array([[14.3, 17.4, 21.2, 4.3, 4, 8.4, 0.5]])
-mu = np.array([6, 7, 9.5, 1.5, 1.3, 3.2, 0])
+mu = np.array([[6, 7, 9.5, 1.5, 1.3, 3.2, 0]])
 rho = np.array(
     [
         [1, 0.82, 0.78, 0.1, 0, 0.5, 0],
@@ -44,11 +44,10 @@ def market_cap(date,n,ent=150):
     for i in range(n):
         if daily_data_cap[i] < boundary:
             res[0,i] = 1
-    print(res)
     return res
 
 
-def weight_cp_mean(sigma, mu, kappa, date):
+def weight_cp_mean(sigma, mu, kappa, date, ent):
 
     n = len(sigma[0])
     m = len(sigma)
@@ -66,12 +65,12 @@ def weight_cp_mean(sigma, mu, kappa, date):
 
     ret2 = cp.norm(np.sqrt(omegaD) @ omegaEigenvectors @ w)
 
-    vol_mean = w.T @ sigma @ w
+    vol_mean = cp.quad_form(w,sigma)
 
-    better_cap = market_cap(date,n,ent)
-    cap_constraint = np.diag(w @ better_cap)
+    #better_cap = market_cap(date,n,ent)
+    #cap_constraint = np.diag(w @ better_cap)
 
-    constraints = [cp.sum(w) == 1, w >= 0, vol_mean <= 0.04, cap_constraint == np.zeros(n,n)]
+    constraints = [cp.sum(w) == 1, w >= 0, vol_mean <= 0.04]
 
     prob = cp.Problem(cp.Maximize(ret - kappa * ret2 ), constraints)
     prob.solve()
@@ -80,13 +79,16 @@ def weight_cp_mean(sigma, mu, kappa, date):
 
 
 
-def portfolio_contraint(date):
-    date_cov = date + " 00:00:00"
-    sigma_mean = main.get_cov(date_cov)
+def portfolio_contraint(date,ent):
+    sigma_mean = main.get_cov(date)
+    print(sigma_mean)
+    if sigma_mean[0][0] == 0:
+        print(sigma_mean)
+        return 0,0,0,0
     mu_mean = np.zeros(len(sigma_mean))
     for i in range(len(sigma_mean)):
         mu_mean[i] = 0.5 * sqrt(sigma_mean[i][i])
-    w_rob,w,prob,ret,ret2 = weight_cp_mean(sigma_mean,mu_mean,0.25,date)
+    w_rob,w,prob,ret,ret2 = weight_cp_mean(sigma_mean,mu_mean,0.25,date,ent)
     return w_rob,w,prob,ret,ret2
 
 
@@ -105,21 +107,42 @@ def weight_cp_mean_lambda(sigma, mu, kappa, date,ent=150):
 
     one = np.ones((n,1))
     lambd = - 1 / (one.T @ inv((sigma+0.000001*np.identity(n))) @ one)
+    lambd = lambd[0][0]
+    print(lambd)
 
     w = cp.Variable(n)
     ret = np.array(mu).T @ w
+    print(ret.shape)
 
     omegaEigenvalues, omegaEigenvectors = np.linalg.eig(omega)
     omegaD = np.diag(omegaEigenvalues)
 
     ret2 = cp.norm(np.sqrt(omegaD) @ omegaEigenvectors @ w)
+    print(ret2.shape)
 
-    better_cap = market_cap(date,n,ent)
-    cap_constraint = np.diag(w @ better_cap)
 
-    constraints = [cp.sum(w) == 1, w >= 0, cap_constraint == np.zeros(n)]
+    #better_cap = market_cap(date,n,ent)
+    
+    #constraints_cap = np.array([w[i] for i in range(len(better_cap[0])) if better_cap[0,i]==1])
 
-    prob = cp.Problem(cp.Maximize(ret - kappa * ret2  - lambd * w.T @ sigma @ w), constraints)
+    #constraints = [cp.sum(w) == 1, w >= 0, constraints_cap == np.array([0]*len(constraints_cap))]
+    
+
+    print(w.shape)
+    #nv_liste = np.array(w).reshape(-1,1)
+    #print(nv_liste.shape)
+    #constraints_cap = np.diag(nv_liste @ better_cap)
+
+
+    #constraints = [cp.sum(w) == 1, w >= 0, sum(constraints_cap @ w) == 0]
+    
+    constraints = [cp.sum(w) == 1, w >= 0]
+
+    ret3 = cp.quad_form(w,sigma)
+    print(ret3.shape)
+    print(lambd.shape)
+
+    prob = cp.Problem(cp.Maximize(ret - kappa * ret2), constraints)
     prob.solve()
 
     return w.value, w, prob, ret, ret2
@@ -144,6 +167,3 @@ def portfolio_contraint_lambda(date,ent):
 
 
 print(portfolio_contraint_lambda("2019-05-31 00:00:00",300))
-
-
-w_prime = np.array([[x] for x in w])
